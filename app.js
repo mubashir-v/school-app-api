9e6a01cb7db915c41c9e0d1f8639de9582a805d4
mubashir-cvr <mubashircheruvayoor@gmail.com>
2025-02-02 01:40:54 +0530

    Port change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 3001;
 
 require("dotenv").config();
 app.use(express.json());
