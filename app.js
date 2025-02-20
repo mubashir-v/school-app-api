b1bb8bd50beaf5fc33216c920c43e7e1ec05c1d7
mubashir-cvr <mubashircheruvayoor@gmail.com>
2025-02-20 18:53:15 +0530

    Changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   next();
 });
 // Example route
-app.use("/", apiRoutes);
+app.use("", apiRoutes);
 
 // Start the server
 app.listen(port, () => {
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,10 +2,10 @@ const authRoutes = require("./auth");
 const express = require("express");
 
 const router = express.Router();
-router.get("", (req, res) => {
+router.get("/", (req, res) => {
   res.send("you are successfuly conected !");
 });
 
-router.use("auth/", authRoutes);
+router.use("/auth", authRoutes);
 
 module.exports = router;
