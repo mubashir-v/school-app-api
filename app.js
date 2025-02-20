81b79cff3abbb24d7cba609e44ca53086bc4c14a
mubashir-cvr <mubashircheruvayoor@gmail.com>
2025-02-20 18:46:31 +0530

    CHanges

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ app.use(express.urlencoded({ extended: true }));
 
 // Enable CORS for all domains
 app.use(cors());
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 // Example route
 app.use("/", apiRoutes);
 
