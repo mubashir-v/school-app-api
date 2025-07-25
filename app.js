d35da961b197bb8eb75edfec5b4010ffef90fa26
Mubashir V <44843238+mubashir-v@users.noreply.github.com>
2025-07-25 11:32:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Enable CORS for all domains
+
 app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
